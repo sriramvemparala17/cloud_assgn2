3ec16e7178f1cb3f3e3d3c3f7bff8c27194c3e1e
sriramvemparala17 <sriramvemparala17@gmail.com>
2022-10-25 20:26:32 -0700

    gcp

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>CMPE281 – Cloud Technology</h1>
+        <h2>Lab #2 Assignment - Google App Engine</h2>
+        <p>
+          Deploying an application into Google App Engine.
+        </p>
+        <img src={logo} alt="Logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
